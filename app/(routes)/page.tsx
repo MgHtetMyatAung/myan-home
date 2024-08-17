5b825996ac98698c59e5f1f0f1aacce25a35b1e0
MgHtetMyatAung <htetmyataung16000@gmail.com>
2024-08-17 12:34:30 +0630

    setup

---
+++ app/(routes)/page.tsx
@@ -0,0 +1,9 @@
+import { Button } from "@/components/ui/button";
+
+export default function Home() {
+  return (
+    <section className=" container mx-auto px-0">
+      <Button>Hello Myan Home</Button>
+    </section>
+  );
+}
---
+++ components/header/Header.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function Header() {
+  return (
+    <header>
+      <nav className=" container mx-auto bg-white p-5 rounded-b-[20px]">
+        <h2 className=" uppercase font-semibold">Myan . Home</h2>
+      </nav>
+    </header>
+  );
+}
